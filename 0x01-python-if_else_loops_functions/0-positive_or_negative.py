35b0a72059852b00c0e25f1dc41b368f70266cef
bonanwaeze@gmail.com <bonanwaeze@gmail.com>
2023-10-31 21:00:36 -0700

    pos or neg num

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
-
 import random
-number = -256 #random.randint(-10000, 10000) # -256 # the_last_digit = -6
-if number < -9:
-    the_last_digit = number % -10
-elif number > 9:
-   the_ last_digit = number % 10
-if the_last_digit > 5:
-    print(f"Last digit of {number} is {last_digit} and is greater than 5")
-elif the_last_digit == 0:
-    print(f"Last digit of {number} is {last_digit} and is zero")
-elif the_last_digit < 6:
-    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
+number = random.randint(-10, 10)
+if number > 0:
+    print(f"{number} is positive")
+elif number == 0:
+    print(f"{number} is zero")
+else:
+    print(f"{number} is negative")
