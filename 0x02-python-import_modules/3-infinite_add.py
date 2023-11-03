9615a42b6ddfab05f02413741c51f3347684a8c5
bonanwaeze@gmail.com <bonanwaeze@gmail.com>
2023-11-02 23:43:43 -0700

    adds nums inifinitely

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+task 3; #!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+sum_of_all = 0
+i = 1
+while i < len(sys.argv):
+    sum_of_all = sum_of_all + int(sys.argv[i])
+    i = i + 1
+print(sum_of_all)
