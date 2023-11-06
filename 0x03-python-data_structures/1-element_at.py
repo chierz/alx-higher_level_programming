730d4f49800f3a2fb0252f959916fd2a2f3c1cb0
bonanwaeze@gmail.com <bonanwaeze@gmail.com>
2023-11-06 09:37:53 -0800

    retrieves an element from a list

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+#Fxn that returns element of a list at a particular index[idx]
+
+def element_at(my_list, idx):
+    for i in my_list:
+        if idx < 0:
+            return None
+        if idx > len(my_list):
+            return None
+        else:
+            return my_list[idx]
+
