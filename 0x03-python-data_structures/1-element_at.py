5f658fa5b58560a39cbc9a294da55b24e7852451
bonanwaeze@gmail.com <bonanwaeze@gmail.com>
2023-11-06 10:46:40 -0800

    retrieves an element from a list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,11 +3,10 @@
 #Fxn that returns element of a list at a particular index[idx]
 
 def element_at(my_list, idx):
-    for i in my_list:
-        if idx < 0:
-            return None
-        elif idx > len(my_list) - 1:
-            return None
-        else:
-            return my_list[idx]
+    if idx < 0:
+        return None
+    elif idx >= len(my_list):
+        return None
+    else:
+        return my_list[idx]
 
